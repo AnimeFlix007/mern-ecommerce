259c14a1a1ae5a672900d3fb0a4d6d38c7a48b25
AnimeFlix007 <devjitbose111@gmail.com>
2023-11-05 19:28:40 +0530

    react-toastify provider added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme/MuiTheme.tsx";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <ToastContainer />
       <App />
     </ThemeProvider>
   </React.StrictMode>
